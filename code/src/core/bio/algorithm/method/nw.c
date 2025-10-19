65dd78ae77c20f1cb6289a111eb26b91afabdd8f
jakovdev <jakodrag345@gmail.com>
2025-10-19 20:50:39 +0200

    Fix/Remove MSVC warnings

--- code/src/core/bio/algorithm/method/nw.c
+++ code/src/core/bio/algorithm/method/nw.c
@@ -13,13 +13,11 @@ align_nw(const sequence_ptr_t seq1, const sequence_ptr_t seq2)
     const sequence_length_t len2 = seq2->length;
 
     size_t matrix_bytes = MATRIX_BYTES(len1, len2);
+    size_t matrix_size = USE_STACK_MATRIX(matrix_bytes) ? MATRIX_SIZE(len1, len2) : 1;
 #ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable : 6255)
-    score_t* stack_matrix = USE_STACK_MATRIX(matrix_bytes) ? (score_t*)_alloca(matrix_bytes) : NULL;
-#pragma warning(pop)
+    score_t* stack_matrix = ALLOCA(stack_matrix, matrix_size);
 #else
-    score_t stack_matrix[USE_STACK_MATRIX(matrix_bytes) ? MATRIX_SIZE(len1, len2) : 1];
+    score_t stack_matrix[matrix_size];
 #endif
     score_t* restrict matrix = matrix_alloc(stack_matrix, matrix_bytes);
 
@@ -68,14 +66,7 @@ align_nw(const sequence_ptr_t seq1, const sequence_ptr_t seq2)
         goto no_stack;
     }
 
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable : 6255)
-#endif
     seq1_indices.data = ALLOCA(seq1_indices.data, len1);
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
     seq1_indices.is_stack = true;
 
 no_stack:
